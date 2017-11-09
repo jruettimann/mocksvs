8c6c14c0a2b7ca3273642a9edd0cc8184a6b3957
Jonas Rüttimann <21096609+jruettimann@users.noreply.github.com>
2017-11-09 16:07:45 +0100

    Add files via upload

---
+++ 2017-10-12/src/main/java/com/mock/empapi/empapimock/data/EmployeeState.java
@@ -0,0 +1,12 @@
+package com.mock.empapi.empapimock.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class EmployeeState {
+
+    private PresenceState presence;
+
+}
---
+++ 2017-10-12/src/main/java/com/mock/empapi/empapimock/data/PresenceState.java
@@ -0,0 +1,7 @@
+package com.mock.empapi.empapimock.data;
+
+public enum PresenceState {
+
+    available, busy, away
+
+}

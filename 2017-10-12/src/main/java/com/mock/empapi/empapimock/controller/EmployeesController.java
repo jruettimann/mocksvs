b1e4db09b57358c5c7ed543dceb472f2fcfdf69e
Jonas Rüttimann <21096609+jruettimann@users.noreply.github.com>
2017-11-09 16:06:33 +0100

    Update EmployeesController.java

--- 2017-10-12/src/main/java/com/mock/empapi/empapimock/controller/EmployeesController.java
+++ 2017-10-12/src/main/java/com/mock/empapi/empapimock/controller/EmployeesController.java
@@ -2,6 +2,8 @@ package com.mock.empapi.empapimock.controller;
 
 import com.mock.empapi.empapimock.data.Dao;
 import com.mock.empapi.empapimock.data.Employee;
+import com.mock.empapi.empapimock.data.EmployeeState;
+import com.mock.empapi.empapimock.data.PresenceState;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
+import java.util.Random;
 
 @RestController
 public class EmployeesController {
@@ -30,20 +33,30 @@ public class EmployeesController {
         try {
             Employee employee = dao.find(id);
             return ResponseEntity.ok(employee);
-        } catch( Exception e) {
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+
+    @GetMapping("/employees/{id}/state")
+    public ResponseEntity<EmployeeState> getState(@PathVariable("id") Integer id) {
+        try {
+            int pick = new Random().nextInt(PresenceState.values().length);
+            PresenceState presence = PresenceState.values()[pick];
+            return ResponseEntity.ok(new EmployeeState(presence));
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
     @GetMapping("/employees/{id}/images/portrait")
     public ResponseEntity<Resource> getPortrait(@PathVariable("id") Integer id) {
         try {
             Employee employee = dao.find(id);
             InputStreamResource resource = new InputStreamResource(employee.getImageFile());
             return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(resource);
-        } catch( Exception e) {
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
-
-
 }

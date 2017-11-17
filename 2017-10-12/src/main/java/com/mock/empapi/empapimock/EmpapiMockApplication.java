3823f4dcbcbbee9a0c82e0fa4b2c6a11f81296c0
Jonas Rüttimann <21096609+jruettimann@users.noreply.github.com>
2017-11-17 15:40:27 +0100

    Add files via upload

--- 2017-10-12/src/main/java/com/mock/empapi/empapimock/EmpapiMockApplication.java
+++ 2017-10-12/src/main/java/com/mock/empapi/empapimock/EmpapiMockApplication.java
@@ -2,11 +2,13 @@ package com.mock.empapi.empapimock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class EmpapiMockApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmpapiMockApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmpapiMockApplication.class, args);
+    }
 }
